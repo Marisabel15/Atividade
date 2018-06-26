15dc32d051281c79862b7602690a79c86b5b7dab
Marisabel15 <srt.ale@gmail.com>
2018-06-26 16:28:39 -0300

    Atividade 3 - Tela de Login

--- SharedPreferencesExample/app/src/main/java/com/example/srtal/sharedpreferencesexample/MainActivity.java
+++ SharedPreferencesExample/app/src/main/java/com/example/srtal/sharedpreferencesexample/MainActivity.java
@@ -4,23 +4,29 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private SharedPreferenceConfig preferenceConfig;
     private EditText UserName,UserPassword;
+    private Button button01;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         preferenceConfig = new SharedPreferenceConfig(getApplicationContext());
+
 
         UserName = findViewById(R.id.user_name);
         UserPassword = findViewById(R.id.user_password);
+        button01 = findViewById(R.id.button_01);
 
 
         if (preferenceConfig.readLoginStatus()){
@@ -43,5 +49,16 @@ public class MainActivity extends AppCompatActivity {
             UserPassword.setText("");
         }
 
+    }
+
+    public void registerUser(View view) {
+        button01.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+            }
+        });
     }
 }
